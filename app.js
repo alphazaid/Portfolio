5583cc09ee2006e1e08845c2959237e77d40304a
Zaid Motiwala <55672326+alphazaid@users.noreply.github.com>
2022-08-19 06:30:41 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -5,3 +5,20 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+function readmore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "block";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "block";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      btnText.style.display = 'none';
+      moreText.style.display = "block";
+    }
+}
+
